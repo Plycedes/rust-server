dc102dab638030b70cc74cb770b4d55705af5d83
Plycedes <abhaypratapyadav340@gmail.com>
2024-01-21 11:28:38 +0530

    reading tcp requests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
 
 fn main() {
     let listner = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -6,6 +8,13 @@ fn main() {
     for stream in listner.incoming(){
         let stream = stream.unwrap();
 
-        println!("Connection Established");
+        connection_handler(stream);
     }
+}
+
+fn connection_handler(mut stream: TcpStream){
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
