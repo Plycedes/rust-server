94eaca017a5cc5f09697acd9fe8054fb67820d87
Plycedes <abhaypratapyadav340@gmail.com>
2024-01-21 12:04:57 +0530

    Reading and Returning HTML response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
@@ -14,16 +15,26 @@ fn main() {
 
 fn connection_handler(mut stream: TcpStream){
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    stream.read(&mut buffer).unwrap();   
 
     // HTTP-Version Status-Code Reason-Phrase CRLF
     // headers CRLF
     // message-body
     // ex: HTTP/1.1 200 OK\r\n\r\n
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status, filename) = 
+    if buffer.starts_with(get){
+        ("HTTP/1.1 200 OK", "./html/index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "./html/404.html")
+    };
+    let content = fs::read_to_string(filename).unwrap();    
+    let response = format!("{}\r\nContent-Length: {}\r\n\r\n{}",status, content.len(), content);
+
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+
+    
 }
